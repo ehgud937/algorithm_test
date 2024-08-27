ecb3629fbcf06acd0c1a668b38d36691b5bcb41e
김도형 <ehgud937@gmail.com>
2024-08-27 11:28:09 +0900

    ,,,

---
+++ algorithm_test/problem_11729.py
@@ -0,0 +1,15 @@
+def hanoi(n, start, end, temp):
+    if n == 1:
+        print(start, end)
+        return
+    
+    hanoi(n-1, start, temp, end)
+    print(start, end)
+    hanoi(n-1, temp, end, start)
+
+
+
+n = int(input())
+
+print((2**n)-1)
+hanoi(n, 1, 3, 2)
